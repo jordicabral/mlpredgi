bb68d6de4ede5613ed4e0ccdd6c4bde605f8ecd9
Jordi Cabral <jordicabral@gmail.com>
2019-12-31 11:02:02 +0100

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' Different variables of 190 genes to compute gene interaction
+#'
+#' An example dataset containing 15 variables of 190 genes
+#'
+#' @format A data frame with 190 rows and 15 variables:
+#'
+#' \describe{
+#'      \item{SGAscore}{Synthetic Genetic Array score, a value between
+#'      -1 and 0}
+#'      \item{PCC}{Pearson Correlation Coefficient}
+#'      \item{NPC}{Networked Partial Correlation}
+#'      \item{fcXX}{Gene Ontology functional information}
+#'      ...
+#'}
+#'@source \url{https://science.sciencemag.org/content/327/5964/425}
+#'@source \url{https://repositori.upf.edu/handle/10230/32383}
+#'@source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC524818/}
+"SGA10_full"
+
+#' Different variables of 635 genes to compute gene interaction
+#'
+#' An example dataset containing 15 variables of 635 genes
+#'
+#' @format A data frame with 635 rows and 15 variables:
+#'
+#' \describe{
+#'      \item{SGAscore}{Synthetic Genetic Array score, a value between
+#'      -1 and 0}
+#'      \item{PCC}{Pearson Correlation Coefficient}
+#'      \item{NPC}{Networked Partial Correlation}
+#'      \item{fcXX}{Gene Ontology functional information}
+#'      ...
+#'}
+#'@source \url{https://science.sciencemag.org/content/353/6306/aaf1420}
+#'@source \url{https://repositori.upf.edu/handle/10230/32383}
+#'@source \url{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC524818/}
+"SGA16_full"
